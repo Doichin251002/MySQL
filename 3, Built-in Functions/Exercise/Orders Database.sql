16bb5d0e6ab5d2258638b749f5fa9055fc154f3f
Doichin251002 <dodi_hristov@abv.bg>
2024-02-12 18:00:49 +0200

    Solutions of problems in Theme: Built-in Functions
    • String Functions;
    • Arithmetical Operators and Numeric Functions;
    • Date Functions;
    • Wildcards.

---
+++ 3, Built-in Functions/Exercise/Orders Database.sql
@@ -0,0 +1,10 @@
+# Query 16. Orders Table
+USE orders;
+
+SELECT
+    `product_name`,
+    `order_date`,
+    DATE_ADD(`order_date`, INTERVAL 3 DAY) AS `pay_due`,
+    DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS `deliver_due`
+FROM
+    orders;
